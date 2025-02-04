e0e10e5243f042adf1c4c95d5e708846c8516d3e
Mu93 <muhammed.abdalhakeem@gmail.com>
2025-02-04 08:43:13 +0200

    fix1

---
+++ src/app/_comp/Dashboard.tsx
@@ -0,0 +1,27 @@
+import JobAlertCard from "@/_components/JobAlertCard";
+import JobList from "@/_components/JobList";
+import Navbar from "@/_components/Navbar";
+import { NavigationMenu } from "@/_components/NavigationMenu";
+import SortingBar from "@/_components/SortingBar";
+
+export default function Dashboard() {
+  return (
+    <div className="flex bg-gray-100 w-full">
+      <div className="hidden md:block w-[15%] ">
+        <NavigationMenu />
+      </div>
+      <div className="flex flex-col relative w-full ">
+        <div className="z-30">
+          <Navbar />
+        </div>
+        <div className="z-20">
+          <SortingBar />
+        </div>
+        <main className="p-6 relative z-10">
+          <JobAlertCard />
+          <JobList />
+        </main>
+      </div>
+    </div>
+  );
+}
